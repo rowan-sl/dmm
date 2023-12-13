a3565c6f0c50cf75a459b2286a8d522d798a05f1
Rowan S-L <r@fawkes.io>
2023-12-13 18:09:46 -0500

    bump version to v0.2.1

--- src/project_meta.rs
+++ src/project_meta.rs
@@ -12,7 +12,7 @@ pub static PROJECT_AUTHORS: &'static str = clap::crate_authors!();
 
 /// Codename associated with the current project version
 pub static PROJECT_VERSION_CODENAME: &'static str = "Margarine";
-const_assert!(const_eq!(clap::crate_version!(), "0.2.0"));
+const_assert!(const_eq!(clap::crate_version!(), "0.2.1"));
 
 lazy_static! {
     pub static ref LOG_ENV: String = format!("{}_LOGLEVEL", PROJECT_NAME);
@@ -34,6 +34,7 @@ pub fn version() -> String {
 {PROJECT_NAME} {GIT_COMMIT_HASH} ({PROJECT_VERSION_CODENAME}) {debug_info}
 Authors: {PROJECT_AUTHORS}
 Repository: https://git.fawkes.io/mtnash/dmm
+License: GNU GPLv3.0
 
 exe: {current_exe_path}"
     )
