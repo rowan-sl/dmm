ee73046905ea0ed07850dfd7d4e9a1455f25cb5f
Rowan S-L <r@fawkes.io>
2023-12-13 17:11:37 -0500

    build flake for all platforms

--- flake.nix
+++ flake.nix
@@ -16,49 +16,50 @@
     };
   };
   outputs = { self, nixpkgs, flake-utils, rust-overlay, crane }:
-    let
-      fs = nixpkgs.lib.fileset;
-      overlays = [ (import rust-overlay ) ];
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system overlays;
-      };
-      rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      # tell crane to use this toolchain
-      craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-      # cf. https://crane.dev/API.html#libcleancargosource
-      # src = craneLib.cleanCargoSource ./.;
-      src = fs.toSource {
-        root = ./.;
-        fileset = fs.unions [
-          ./assets/dmm.default.ron
-          (fs.fromSource (craneLib.cleanCargoSource ./.))
-        ];
-      };
-      # compile-time
-      nativeBuildInputs = with pkgs; [ rustToolchain clang mold-wrapped pkg-config alsa-lib ];
-      # runtime
-      buildInputs = with pkgs; [ yt-dlp alsa-lib ]; # needed system libraries
-      cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs nativeBuildInputs; };
-      bin = craneLib.buildPackage ({ inherit src buildInputs nativeBuildInputs cargoArtifacts; });
-    in
-    {
-      packages.${system} = {
-        # so bin can be spacifically built, or just by default
-        inherit bin;
-        default = bin;
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        inherit buildInputs;
-        name = "dmm-dev";
-        nativeBuildInputs = [
-          pkgs.rust-analyzer-unwrapped
-          pkgs.nodePackages.vscode-langservers-extracted
-        ] ++ nativeBuildInputs;
-        RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
-        shellHook = ''
-        exec zsh
-        '';
-      };
-    };
+    flake-utils.lib.eachDefaultSystem(system:
+      let
+        fs = nixpkgs.lib.fileset;
+        overlays = [ (import rust-overlay ) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        # tell crane to use this toolchain
+        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
+        # cf. https://crane.dev/API.html#libcleancargosource
+        # src = craneLib.cleanCargoSource ./.;
+        src = fs.toSource {
+          root = ./.;
+          fileset = fs.unions [
+            ./assets/dmm.default.ron
+            (fs.fromSource (craneLib.cleanCargoSource ./.))
+          ];
+        };
+        # compile-time
+        nativeBuildInputs = with pkgs; [ rustToolchain clang mold-wrapped pkg-config alsa-lib ];
+        # runtime
+        buildInputs = with pkgs; [ yt-dlp alsa-lib ]; # needed system libraries
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src buildInputs nativeBuildInputs; };
+        bin = craneLib.buildPackage ({ inherit src buildInputs nativeBuildInputs cargoArtifacts; });
+      in
+      {
+        packages = {
+          # so bin can be spacifically built, or just by default
+          inherit bin;
+          default = bin;
+        };
+        devShells.default = pkgs.mkShell {
+          inherit buildInputs;
+          name = "dmm-dev";
+          nativeBuildInputs = [
+            pkgs.rust-analyzer-unwrapped
+            pkgs.nodePackages.vscode-langservers-extracted
+          ] ++ nativeBuildInputs;
+          RUST_SRC_PATH = "${rustToolchain}/lib/rustlib/src/rust/library";
+          shellHook = ''
+          exec zsh
+          '';
+        };
+      }
+    );
 }
